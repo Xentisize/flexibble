b0674b544deac5e22717d83c8662326065ec1d76
Xentisize <xentisize@gmail.com>
2023-07-25 07:58:07 +0800

    Working on AuthProviders

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,36 +5,36 @@ import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
-  const session = {};
+	const session = null;
 
-  return (
-    <nav className="flexBetween navbar">
-      <div className="flex-1 flextStart gap-10">
-        <Link href="/">
-          <Image src="/logo.svg" width={115} height={43} alt="Flexibble"></Image>
-        </Link>
+	return (
+		<nav className="flexBetween navbar">
+			<div className="flex-1 flextStart gap-10">
+				<Link href="/">
+					<Image src="/logo.svg" width={115} height={43} alt="Flexibble"></Image>
+				</Link>
 
-        <ul className="xl:flex hidden text-small gap-7">
-          {NavLinks.map((link) => (
-            <Link href={link.href} key={link.key}>
-              {link.text}
-            </Link>
-          ))}
-        </ul>
-      </div>
+				<ul className="xl:flex hidden text-small gap-7">
+					{NavLinks.map((link) => (
+						<Link href={link.href} key={link.key}>
+							{link.text}
+						</Link>
+					))}
+				</ul>
+			</div>
 
-      <div className="flexCenter gap-4">
-        {session ? (
-          <>
-            UserPhoto
-            <Link href="/create-project">Share work</Link>
-          </>
-        ) : (
-          <AuthProviders />
-        )}
-      </div>
-    </nav>
-  );
+			<div className="flexCenter gap-4">
+				{session ? (
+					<>
+						UserPhoto
+						<Link href="/create-project">Share work</Link>
+					</>
+				) : (
+					<AuthProviders />
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
